b8be4b22ad771eaf159076276e532e52f6ae7577
andfed <a.fedorov@rdpin.ru>
2025-11-30 14:05:31 +0300

    [A] lab 3

---
+++ src/tb/scr1_tb_log_cmd.sv
@@ -0,0 +1,27 @@
+
+module scr1_tb_log_cmd();
+
+
+always_ff @(posedge scr1_top_tb_ahb.i_top.i_imem_ahb.clk) begin
+    if (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_resp == 2'b01) begin
+        // valid data from ahb router
+        if (
+            (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata[6 : 0] == 7'b0110011) &    // R-type
+            // (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata[14 : 12] == 3'b111)
+            (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata[14 : 12] == 3'b000) &      // funct3: 000 - SUB
+            (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata[31 : 25] == 7'b0100000)        // funct7: 0100000 - SUB
+        ) begin
+            // detect and command
+            $display("=== DETECTED SUB COMMAND ===");
+            $display("Time: %0t", $time);
+            // $display("ahb_imem_req_ack: 0x%h", scr1_top_tb_ahb.i_top.i_imem_ahb.ahb_imem_req_ack);
+            $display("imem_rdata: 0x%h", scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata);
+            $display("imem_resp: 0x%h", scr1_top_tb_ahb.i_top.i_imem_ahb.imem_resp);
+            $display("mepc: 0x%h", scr1_top_tb_ahb.i_top.i_core_top.i_pipe_top.i_pipe_csr.csr_mepc);
+            $display("curr_pc: 0x%h", scr1_top_tb_ahb.i_top.i_core_top.i_pipe_top.curr_pc);
+            $display("=================================");
+        end
+    end
+end
+
+endmodule
